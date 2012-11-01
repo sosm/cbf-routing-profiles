37f2c6536ac26ff36abf33492c012022767614d9
Sarah Hoffmann <lonvia@denofr.de>
2012-11-02 00:15:32 +0100

    add subprofiles for hikers and normal pedestrians

--- foot-city.lua
+++ foot-city.lua
@@ -1,3 +1,6 @@
+-- FOOT profile for city walkers - preference for paved underground
+--
+--
 require("tags")
 require("barrier")
 require("highway")
@@ -7,14 +10,14 @@ require("transport")
 --
 ignore_areas 			= true -- future feature
 traffic_signal_penalty 	= 2
-u_turn_penalty 			= 20
-use_restrictions        = true
+u_turn_penalty 			= 2
+use_restrictions        = false
 
 --
 -- Globals for profile definition
 --
 
-local access_list = { "bicycle", "vehicle", "access" }
+local access_list = { "foot", "access" }
 
 
 ---------------------------------------------------------------------------
@@ -33,7 +36,13 @@ local barrier_access = {
     ["cattle_grid"] = true,
     ["entrance"] = true,
     ["sally_port"] = true,
-    ["toll_both"] = true
+    ["toll_both"] = true,
+    ["cycle_barrier"] = true,
+    ["stile"] = true,
+    ["block"] = true,
+    ["kissing_gate"] = true,
+    ["turnstile"] = true,
+    ["hampshire_gate"] = true
 }
 
 function node_function (node)
@@ -66,47 +75,48 @@ end
 --
 -- Begin of globals
 
-local default_speed = 16
-local designated_speed = 18
-local speed_highway = { 
-	["cycleway"] = 18,
-	["primary"] = 15,
-	["primary_link"] = 15,
-	["secondary"] = 16,
-	["secondary_link"] = 16,
-	["tertiary"] = 17,
-	["tertiary_link"] = 17,
-	["residential"] = 18,
-	["unclassified"] = 18,
-	["living_street"] = 18,
-	["road"] = 17,
-	["service"] = 17,
-	["path"] = 16,
-	["footway"] = 5,
-	["pedestrian"] = 5,
-	["steps"] = 1,
+local default_speed = 10
+local designated_speed = 12
+local speed_highway = {
+    ["footway"] = 12,
+	["cycleway"] = 10,
+	["primary"] = 7,
+	["primary_link"] = 7,
+	["secondary"] = 8,
+	["secondary_link"] = 8,
+	["tertiary"] = 9,
+	["tertiary_link"] = 9,
+	["residential"] = 10,
+	["unclassified"] = 10,
+	["living_street"] = 11,
+	["road"] = 10,
+	["service"] = 10,
+	["path"] = 12,
+	["pedestrian"] = 12,
+	["steps"] = 11,
 }
 
-local speed_track = { 18, 16, 16 }
+local speed_track = { 11, 11, 10, 9, 9 }
 
 local speed_path = {
-    sac_scale = nil,
-    foot = { designated = 0.5,
-             yes = 0.9 }
+    sac_scale = { hiking = 0.5,
+                  mountain_hiking = 0,
+                  demanding_mountain_hiking = 0,
+                  alpine_hiking = 0,
+                  demanding_alpine_hiking = 0
+                },
+    bicycle = { designated = 0.5, yes = 0.9 }
 }
 
 local surface_penalties = { 
-    ["gravel"] = 0.8,
+    ["gravel"] = 0.7,
     ["ground"] = 0.8,
-    ["unpaved"] = 0.9,
+    ["unpaved"] = 0.8,
     ["grass"] = 0.5,
     ["dirt"] = 0.5,
-    ["cobblestone"] = 0.5, 
-    ["pebblestone"] = 0.9, 
-    ["compacted"] = 0.9, 
-    ["wood"] = 0.95,
-    ["grit"] = 0.6,
-    ["sand"] = 0.4
+    ["compacted"] = 0.9,
+    ["grit"] = 0.8,
+    ["sand"] = 0.6
 }
 
 local name_list = { "ref", "name" }
@@ -117,7 +127,7 @@ function way_function (way, numberOfNodesInWay)
 		return 0;
 	end
 
- 	-- Check if we are allowed to access the way
+    -- Check if we are allowed to access the way
     if tags.get_access_grade(way.tags, access_list) < -1 then
 		return 0
     end
@@ -130,7 +140,7 @@ function way_function (way, numberOfNodesInWay)
     -- is it a valid highway?
     if not highway.set_base_speed(way, speed_highway, speed_track) then
         -- check for designated access
-        if tags.as_access_grade(way.tags:Find('bicycle')) > 0 then
+        if tags.as_access_grade(way.tags:Find('foot')) > 0 then
             way.speed = default_speed
         else
             return 0
@@ -144,19 +154,15 @@ function way_function (way, numberOfNodesInWay)
         return 0
     end
 
-    local cycleway = way.tags:Find('cycleway')
-    if (cycleway == 'lane' or cycleway == 'track') and
-         (highway == 'primary' or highway == 'secondary') then
-        way.speed = way.speed + 1
+    -- if there is a sidewalk, the better
+    local sidewalk = way.tags:Find('sidewalk')
+    if sidewalk == 'both' or sidewalk == 'left' or sidewalk == 'right' then
+        way.speed = default_speed
     end
 
-    -- finally: restrict to maxspeed
-    highway.restrict_to_maxspeed(way, 1.0)
-
-    -- Set direction according to tags on way
-    highway.set_directions(way, "bicycle")
-	if tags.as_oneway(way.tags:Find("cycleway")) == Way.opposite then
-        way.direction = Way.bidirectional
+    local junction = way.tags:Find('junction')
+    if junction == "roundabout" then
+        way.roundabout = true
     end
   
     way.name = tags.get_name(way.tags, name_list)
--- foot-hiking.lua
+++ foot-hiking.lua
@@ -1,3 +1,6 @@
+-- FOOT profile for hikers - preference for unpaved underground
+--
+--
 require("tags")
 require("barrier")
 require("highway")
@@ -7,14 +10,14 @@ require("transport")
 --
 ignore_areas 			= true -- future feature
 traffic_signal_penalty 	= 2
-u_turn_penalty 			= 20
-use_restrictions        = true
+u_turn_penalty 			= 2
+use_restrictions        = false
 
 --
 -- Globals for profile definition
 --
 
-local access_list = { "bicycle", "vehicle", "access" }
+local access_list = { "foot", "access" }
 
 
 ---------------------------------------------------------------------------
@@ -33,7 +36,13 @@ local barrier_access = {
     ["cattle_grid"] = true,
     ["entrance"] = true,
     ["sally_port"] = true,
-    ["toll_both"] = true
+    ["toll_both"] = true,
+    ["cycle_barrier"] = true,
+    ["stile"] = true,
+    ["block"] = true,
+    ["kissing_gate"] = true,
+    ["turnstile"] = true,
+    ["hampshire_gate"] = true
 }
 
 function node_function (node)
@@ -66,47 +75,41 @@ end
 --
 -- Begin of globals
 
-local default_speed = 16
-local designated_speed = 18
-local speed_highway = { 
-	["cycleway"] = 18,
-	["primary"] = 15,
-	["primary_link"] = 15,
-	["secondary"] = 16,
-	["secondary_link"] = 16,
-	["tertiary"] = 17,
-	["tertiary_link"] = 17,
-	["residential"] = 18,
-	["unclassified"] = 18,
-	["living_street"] = 18,
-	["road"] = 17,
-	["service"] = 17,
-	["path"] = 16,
-	["footway"] = 5,
-	["pedestrian"] = 5,
-	["steps"] = 1,
+local default_speed = 10
+local designated_speed = 12
+local speed_highway = {
+    ["footway"] = 12,
+	["cycleway"] = 10,
+	["primary"] = 7,
+	["primary_link"] = 7,
+	["secondary"] = 8,
+	["secondary_link"] = 8,
+	["tertiary"] = 9,
+	["tertiary_link"] = 9,
+	["residential"] = 10,
+	["unclassified"] = 10,
+	["living_street"] = 11,
+	["road"] = 10,
+	["service"] = 10,
+	["path"] = 12,
+	["pedestrian"] = 12,
+	["steps"] = 11,
 }
 
-local speed_track = { 18, 16, 16 }
+local speed_track = { 10, 11, 11, 11, 11 }
 
 local speed_path = {
-    sac_scale = nil,
-    foot = { designated = 0.5,
-             yes = 0.9 }
+    sac_scale = { mountain_hiking = 0.9,
+                  demanding_mountain_hiking = 0.5,
+                  alpine_hiking = 0,
+                  demanding_alpine_hiking = 0
+                },
+    bicycle = { designated = 0.5, yes = 0.9 }
 }
 
 local surface_penalties = { 
-    ["gravel"] = 0.8,
-    ["ground"] = 0.8,
-    ["unpaved"] = 0.9,
-    ["grass"] = 0.5,
-    ["dirt"] = 0.5,
-    ["cobblestone"] = 0.5, 
-    ["pebblestone"] = 0.9, 
-    ["compacted"] = 0.9, 
-    ["wood"] = 0.95,
-    ["grit"] = 0.6,
-    ["sand"] = 0.4
+    ["gravel"] = 0.9,
+    ["paved"] = 0.9
 }
 
 local name_list = { "ref", "name" }
@@ -117,7 +120,7 @@ function way_function (way, numberOfNodesInWay)
 		return 0;
 	end
 
- 	-- Check if we are allowed to access the way
+    -- Check if we are allowed to access the way
     if tags.get_access_grade(way.tags, access_list) < -1 then
 		return 0
     end
@@ -130,7 +133,7 @@ function way_function (way, numberOfNodesInWay)
     -- is it a valid highway?
     if not highway.set_base_speed(way, speed_highway, speed_track) then
         -- check for designated access
-        if tags.as_access_grade(way.tags:Find('bicycle')) > 0 then
+        if tags.as_access_grade(way.tags:Find('foot')) > 0 then
             way.speed = default_speed
         else
             return 0
@@ -144,19 +147,15 @@ function way_function (way, numberOfNodesInWay)
         return 0
     end
 
-    local cycleway = way.tags:Find('cycleway')
-    if (cycleway == 'lane' or cycleway == 'track') and
-         (highway == 'primary' or highway == 'secondary') then
-        way.speed = way.speed + 1
+    -- if there is a sidewalk, the better
+    local sidewalk = way.tags:Find('sidewalk')
+    if sidewalk == 'both' or sidewalk == 'left' or sidewalk == 'right' then
+        way.speed = default_speed
     end
 
-    -- finally: restrict to maxspeed
-    highway.restrict_to_maxspeed(way, 1.0)
-
-    -- Set direction according to tags on way
-    highway.set_directions(way, "bicycle")
-	if tags.as_oneway(way.tags:Find("cycleway")) == Way.opposite then
-        way.direction = Way.bidirectional
+    local junction = way.tags:Find('junction')
+    if junction == "roundabout" then
+        way.roundabout = true
     end
   
     way.name = tags.get_name(way.tags, name_list)
